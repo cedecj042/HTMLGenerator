cf0fe8996a39948a938f17348d612810da21fbd0
Cedric <cedricoporto1st@gmail.com>
2024-05-12 22:16:49 +0800

    loader changes

--- loader.php
+++ loader.php
@@ -7,11 +7,3 @@
             require_once $file;
         }
     });
-
-    // spl_autoload_register(function($class){
-    //     $classFile = $class . '.php';
-
-    //     if(is_file($classFile) and !class_exists($class))
-    //         require_once $classFile;
-    // });
-    
